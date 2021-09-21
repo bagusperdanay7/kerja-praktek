a5fe45b98402275745fedf72b9084ecad133faca
Robi Nurhidayat <robivanpersie290899@gmail.com>
2021-09-22 06:31:33 +0700

    membuat table deployment

--- kerja-praktek/resources/views/rekap/index.blade.php
+++ kerja-praktek/resources/views/rekap/index.blade.php
@@ -1,9 +1,10 @@
 @extends('template.main')
 
 @section('contain')
+<div class="container">
     <div class="row">
         <div class="col-md-12 col-lg-12">
-            <div class="card mt-3 mx-3">
+            <div class="card mt-3">
                 <div class="card-header">
                     <div class="button-export mt-4">
                         <h1>Data Rekap</h1>
@@ -94,6 +95,7 @@
                 </div>
             </div>
         </div>
+       </div>
     </div>
 </div>
 @endsection
