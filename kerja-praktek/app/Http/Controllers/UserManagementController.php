ceaa65c98cfbff001456eace76f9d6d95ccbf516
Technoice Developer <icetech.official@gmail.com>
2021-09-27 13:26:44 +0700

    email ganti nama

--- kerja-praktek/app/Http/Controllers/UserManagementController.php
+++ kerja-praktek/app/Http/Controllers/UserManagementController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserManagementController extends Controller
 {
     //
+    public function index()
+    {
+        $user = DB::table('users')->get();
+        return view('usermanagement.usercontroller', compact('user'));
+    }
 }
