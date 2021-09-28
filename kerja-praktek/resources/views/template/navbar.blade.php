588a8fbe0964de97fe76524b84177d4d4d76a063
Bagus Perdana <bagus.10118249@mahasiswa.unikom.ac.id>
2021-09-28 13:32:17 +0700

    Ngisi Nav Link

--- kerja-praktek/resources/views/template/navbar.blade.php
+++ kerja-praktek/resources/views/template/navbar.blade.php
@@ -33,7 +33,7 @@
                             </a>
                             <div class="dropdown-menu dropmenu" aria-labelledby="progressMenu">
                                 <a class="dropdown-item" href="{{ route('progress.create') }}"><i class="las la-plus mr-3"></i>New Progress</a>
-                                <a class="dropdown-item" href="#"><i class="las la-pen mr-3"></i>Update Progress</a>
+                                <a class="dropdown-item" href="{{ route('progress.index') }}"><i class="las la-pen mr-3"></i>Update Progress</a>
                             </div>
                         </li>
                         <li class="nav-item dropdown">
